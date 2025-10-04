0d31c3a9fe9f8ee0b55b84950d8e2dede477745d
Gayatri Jambhale <gayatrijambhale7@gmail.com>
2025-10-04 18:05:39 +0530

    Update TodoList.jsx

--- TodoList.jsx
+++ TodoList.jsx
@@ -1,80 +1,106 @@
 import { useState } from "react";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
-export default function TodoList(){
-    let [todos,setTodos]=useState([{task:"Sample list" , id:uuidv4() , isDone:false}])
-    let[newtodo,setNewTodo]=useState([""])
+export default function TodoList() {
+  let [todos, setTodos] = useState([
+    { task: "Sample list", id: uuidv4(), isDone: false },
+  ]);
+  let [newtodo, setNewTodo] = useState("");
 
-    let addnewlist=() => {
-        setTodos([...todos,{ task:newtodo, id:uuidv4() , isDone:false}]);
-        setNewTodo("");
-    }
+  let addnewlist = () => {
+    if (newtodo.trim() === "") return;
+    setTodos([...todos, { task: newtodo, id: uuidv4(), isDone: false }]);
+    setNewTodo("");
+  };
 
-    let updatetodolist=(event) => {
-        // console.log(event.target.value);
-        setNewTodo(event.target.value);
-    }
+  let updatetodolist = (event) => {
+    setNewTodo(event.target.value);
+  };
 
-    let deletetodos=(id) => {
-        // setTodos((prevesTodo) => todos.filter((prevTodo) => prevesTodo.id != id));
-        // console.log("todos deleted");
-        setTodos(todos.filter((todo) => todo.id != id));
-    }
+  let deletetodos = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
 
-    let MarkDoneall= () => {
-        setTodos(todos.map((todo) => {
-        return{
-            ...todo,
-            // task:todo.task.toUpperCase(),
-            isDone:true,
+  let MarkDoneall = () => {
+    setTodos(
+      todos.map((todo) => {
+        return {
+          ...todo,
+          isDone: true,
         };
-         
-       }));
-    }
+      })
+    );
+  };
 
-     let Markasdone= (id) => {
-        setTodos(todos.map((todo) => {
-            if(todo.id==id){
-                return{
-                    ...todo,
-                    // task:todo.task.toUpperCase(),
-                    isDone:true,
-                };
-            }else{
-                return todo;
-            }
-       }));
-    }
+  let Markasdone = (id) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            isDone: true,
+          };
+        } else {
+          return todo;
+        }
+      })
+    );
+  };
 
+  return (
+    <div className="container mt-5 p-4 border rounded shadow-lg bg-light" style={{ maxWidth: "500px" }}>
+      <h2 className="text-center mb-4 text-primary">📝 To-Do List</h2>
 
+      <div className="input-group mb-3">
+        <input
+          placeholder="Add a new task"
+          value={newtodo}
+          onChange={updatetodolist}
+          className="form-control"
+        />
+        <button onClick={addnewlist} className="btn btn-success">
+          Add
+        </button>
+      </div>
 
-    return(
-        <div>
-            <input placeholder="Add to do task" value={newtodo} onChange={updatetodolist} ></input>
-            <br></br>
-            <br></br>
-            <button onClick={addnewlist}>Add list </button>
-            <br></br><br></br>
-            <hr></hr>
-            <h3> To do list </h3>
-            <ul>
-                {
-                todos.map((todo) => {
-                    return <li key={todo.id}>
-                        <span style={todo.isDone ? {color:"red"} : {}}>{todo.task}</span>
-                        &nbsp;&nbsp;&nbsp;
-                        <button onClick={ () => deletetodos(todo.id)}>Delete</button>
-                        &nbsp;&nbsp;&nbsp;
-                         <button onClick={ () =>  Markasdone(todo.id)}>Mark as Done</button>
-                    </li>;
-                    console.log(todo);
-                })
-            }
-            </ul>
-            <br/><br/>
-            <button onClick={MarkDoneall}>Mark Done All</button>
-        </div>
-    )
+      <hr />
+
+      <h5 className="text-secondary">Your Tasks:</h5>
+      <ul className="list-group">
+        {todos.map((todo) => (
+          <li
+            key={todo.id}
+            className="list-group-item d-flex justify-content-between align-items-center"
+          >
+            <span
+              style={todo.isDone ? { textDecoration: "line-through", color: "gray" } : {}}
+            >
+              {todo.task}
+            </span>
+            <div>
+              <button
+                onClick={() => Markasdone(todo.id)}
+                className="btn btn-sm btn-outline-primary me-2"
+                disabled={todo.isDone}
+              >
+                Done
+              </button>
+              <button
+                onClick={() => deletetodos(todo.id)}
+                className="btn btn-sm btn-outline-danger"
+              >
+                Delete
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
+
+      <div className="text-center mt-4">
+        <button onClick={MarkDoneall} className="btn btn-warning">
+          Mark All as Done
+        </button>
+      </div>
+    </div>
+  );
 }
-
-
